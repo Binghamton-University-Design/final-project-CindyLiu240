27e11a68d4b46b55832c4a0fde6f95a6c35f0513
Kevin Lahoda <klahoda@binghamton.edu>
2022-04-21 12:42:09 -0400

    slider integration

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,23 @@
 /* javascript */
 
-$(document).ready(function() {
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
-console.log('hello');
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
